ec3c2d78614bb4acbc13821b0486d0c873b4895c
atomiomi <atomio.ak@gmail.com>
2021-12-22 22:28:43 +0600

    Api start

--- pages/api/quiz.page.js
+++ pages/api/quiz.page.js
@@ -1,28 +1,20 @@
-const handler = async (req, res) => {
-  res.status(200).json([
-    {
-      code: '',
-      title: 'What\'s the output?',
-      answer: 'A',
-      options: [
-        { key: 'A', value: 'Target > Capturing > Bubbling' },
-        { key: 'B', value: 'Target > Capturing > Bubbling' },
-        { key: 'C', value: 'Target > Capturing > Bubbling' },
-        { key: 'D', value: 'Target > Capturing > Bubbling' },
-      ]
-    },
-    {
-      code: '',
-      title: 'What\'s the output???',
-      answer: 'B',
-      options: [
-        { key: 'A', value: 'Capturing > Capturing > Bubbling' },
-        { key: 'B', value: 'Target > Capturing > Bubbling' },
-        { key: 'C', value: 'Target > Capturing > Bubbling' },
-        { key: 'D', value: 'Target > Capturing > Bubbling' },
-      ]
-    }
-  ])
+import fetch from 'node-fetch';
+
+const handler = async (req, res, next) => {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/lydiahallie/javascript-questions/master/README.md');
+    let rawData = await response.text();
+
+    rawData = rawData.replace(/^(.*?)##/s, '##');
+    const sections = rawData.split('######');
+
+    res.json({
+      sections,
+    });
+  } catch(err) {
+    console.error('Error while loading data:', err);
+    res.status(500).end();
+  }
 };
 
 export default handler;
